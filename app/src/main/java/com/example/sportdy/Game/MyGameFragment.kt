27b97e894174f9d1dba5d8ce666de5ce20f9db6b
liow122 <liowzm-wm18@student.tarc.edu.my>
2020-01-06 11:08:15 +0800

    .

--- app/src/main/java/com/example/sportdy/Game/MyGameFragment.kt
+++ app/src/main/java/com/example/sportdy/Game/MyGameFragment.kt
@@ -2,25 +2,37 @@ package com.example.sportdy.Game
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.viewpager2.widget.ViewPager2
-
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.sportdy.R
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_my_game.*
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class MyGameFragment : Fragment() {
 
-    override fun onCreateView(
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        fabHostGame.setOnClickListener(){
+
+        }
+    }
+    override fun onCreateView (
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_game, container, false)
+
+
     }
 
 }
+
